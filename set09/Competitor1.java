2c0ec22f27fd9fd23108c09707758ff80a988ef6
Xiaoming <wu.xiaom@husky.neu.edu>
2017-11-10 18:27:53 -0500

    strugling on power ranking

--- set09/Competitor1.java
+++ set09/Competitor1.java
@@ -1,7 +1,9 @@
 // Constructor template for Competitor1:
 //     new Competitor1 (Competitor c1)
+//     new Competitor1 (Competitor c1, Boolean outranked)
 //
 // Interpretation: the competitor represents an individual or team
+// the outranked represents a mark and true if this competitor has been visited
 
 // Note:  In Java, you cannot assume a List is mutable, because all
 // of the List operations that change the state of a List are optional.
@@ -16,16 +18,26 @@ import java.util.stream.Collectors;
 class Competitor1 implements Competitor {
 
     String n;  // the name of this competitor
-    Boolean isOutranked;
-
+    Boolean isMarked; // the visited mark of this competitor
+    int outranksNum;
+    int outrankedNum;
+    double percentage;
     Competitor1 (String s) {
         n = s;
-        isOutranked = false;
+        isMarked = false;
     }
 
-    Competitor1 (String s, Boolean isOutranked) {
+    Competitor1 (String s, Boolean isMarked) {
         n = s;
-        this.isOutranked =  isOutranked;
+        this.isMarked =  isMarked;
+    }
+
+    Competitor1 (String s, List<Outcome> outcomes){
+        n = s;
+        isMarked = false;
+        outrankedNum = outrankedBy(outcomes).size();
+        outranksNum = outranks(outcomes).size();
+        percentage = noLosingPercentage(outcomes);
     }
 
     // returns the name of this competitor
@@ -59,16 +71,15 @@ class Competitor1 implements Competitor {
     public List<String> outranks (List<Outcome> outcomes) {
         Set<Competitor>names = onDefeatedOrTied(outcomes);
         List<String> ns = new ArrayList();
+        List<Outcome> marked_outcomes;
         Consumer<Competitor> toString = (Competitor p) -> ns.add(p.name());
         names.forEach(toString);
-
-        if (isOutranked){
+        if (isMarked){
             return ns;
         }else {
-            isOutranked = true;
-            markOutcome(outcomes);
+            marked_outcomes = markOutcome(outcomes);
             for (Competitor c : names){
-                ns.addAll(c.outranks(outcomes));
+                ns.addAll(c.outranks(marked_outcomes));
             }
         }
         return ns.stream().distinct().collect(Collectors.toList());
@@ -78,20 +89,20 @@ class Competitor1 implements Competitor {
     // RETURNS: a list of the names of all competitors mentioned by
     //     the outcomes that outrank this competitor,
     //     without duplicates, in alphabetical order
-
     public List<String> outrankedBy (List<Outcome> outcomes) {
         Set<Competitor>names = onDefeatedOrTiedBy(outcomes);
         List<String> ns = new ArrayList();
+        List<Outcome> marked_outcomes;
         for (Competitor c : names){
             ns.add(c.name());
         }
-        if (isOutranked){
+        if (isMarked){
             return ns;
         }else {
-            isOutranked = true;
-            markOutcome(outcomes);
+            isMarked = true;
+            marked_outcomes =  markOutcome(outcomes);
             for (Competitor c : names){
-                ns.addAll(c.outrankedBy(outcomes));
+                ns.addAll(c.outrankedBy(marked_outcomes));
             }
         }
         return ns.stream().distinct().collect(Collectors.toList());
@@ -104,41 +115,84 @@ class Competitor1 implements Competitor {
     //     competitor B in the list if and only if the power-ranking
     //     of A is higher than the power ranking of B.
     public List<String> powerRanking (List<Outcome> outcomes) {
-        throw new UnsupportedOperationException();
+            List<Competitor> competitors = competitorsOfoutcomes(outcomes);
+            List<Competitor> markedCompetitors = new ArrayList();
+
+         markedCompetitors = competitors.stream()
+                .filter(p -> p.).collect(Collectors.toList());
+        outrankedNum = outrankedBy(outcomes).size();
+        outranksNum = outranks(outcomes).size();
+        percentage = noLosingPercentage(outcomes);
+        isMarked = true;
+        if (isMarked){
+
+        }
+        List<Outcome> mark_outcomes = markOutcome(outcomes);
+        for (Competitor c : competitors){
+            c.powerRanking(outcomes);
+        }
+        competitors.sort((c1,c2) ->{
+        return c1.outrankedBy(outcomes).size() < c2.outrankedBy(outcomes).size()? -1:1;});
+        competitors.sort((c1,c2) ->{
+                return c1.outrankedBy(outcomes).size() == c2.outrankedBy(outcomes).size() &&
+                        c1.outranks(outcomes).size() > c2.outranks(outcomes).size()? 1:-1;
+           });
+        List<Outcome> beerDrinkers = outcomes.stream()
+                .filter(p -> isMention(p)).collect(Collectors.toList());
+        competitors.sort((c1,c2) ->{
+            return c1.outrankedBy(outcomes).size() == c2.outrankedBy(outcomes).size() &&
+                    c1.outranks(outcomes).size() == c2.outranks(outcomes).size()
+                    ? 1:-1;
+        });
+        return null;
+    }
+
+
+
+
+    private List<Competitor> competitorsOfoutcomes(List<Outcome> outcomes){
+        Set<Competitor> names = new HashSet();
+        for (Outcome o : outcomes){
+                names.add(o.first());
+                names.add(o.second());
+        }
+        return new ArrayList(names);
     }
 
     /*
     Given: a list of outcomes
     Returns: a list like the given one except changing this competitor
-    to a marked one (set isOutranked to true)
+    to a marked one (set isMarked to true)
      */
-    private void markOutcome(List<Outcome> outcomes){
-        for (int i = 0; i < outcomes.size(); i++){
+    private List<Outcome> markOutcome(List<Outcome> outcomes){
+        List<Outcome> olst = new ArrayList();
+        for (Outcome o : outcomes){
             Competitor c1;
             Competitor c2;
-            if (outcomes.get(i).isTie() && isMemberInTie(outcomes.get(i))){
-                if (outcomes.get(i).first().equals(this)){
-                    c1 = competitorOutranked(outcomes.get(i).first().name());
-                    c2 = outcomes.get(i).second();
+            if (o.isTie() && isMemberInTie(o)){
+                if (o.first().equals(this)){
+                    c1 = competitorOutranked(o.first().name());
+                    c2 = o.second();
                 }else {
-                    c1 = outcomes.get(i).first();
-                    c2 = competitorOutranked(outcomes.get(i).second().name());
+                    c1 = o.first();
+                    c2 = competitorOutranked(o.second().name());
                 }
-                outcomes.set(i,new Tie1(c1,c2));
-            }else if(!outcomes.get(i).isTie()) {
-                if (outcomes.get(i).winner().equals(this)){
-                     c1 = outcomes.get(i).loser();
-                     c2 = competitorOutranked(outcomes.get(i).winner().name());
-                }else if (outcomes.get(i).loser().equals(this)){
-                    c1 = competitor(outcomes.get(i).loser().name());
-                    c2 = outcomes.get(i).winner();
+                olst.add(new Tie1(c1,c2));
+            }else if(!o.isTie() && isMention(o)) {
+                if (o.winner().equals(this)){
+                    c1 = o.loser();
+                    c2 = competitorOutranked(o.winner().name());
+                }else {
+                    c1 = competitorOutranked(o.loser().name());
+                    c2 = o.winner();
+                }
+                     olst.add(new Defeat1(c2,c1));
                 }
                 else {
-                    continue;
+                    olst.add(o);
                 }
-                outcomes.set(i,new Defeat1(c2,c1));
             }
-        }
+        return olst;
     }
    
     /*
@@ -188,6 +242,40 @@ class Competitor1 implements Competitor {
         return o.first().equals(this) || o.second().equals(this);
     }
 
+    /*
+    Given: a list of outcomes
+    Returns: the number of outcomes in which this competitordefeats or ties 
+       another competitor divided by the number of outcomes that mention this competitor
+     */
+    private double noLosingPercentage(List<Outcome> olst){
+        int outranksNum = 0;
+        int mentionNum = 0;
+        for(Outcome o : olst){
+            if (defeatedOrTie(o))
+                outranksNum++;
+            
+            else if (isMention(o))
+                mentionNum++;
+        }
+        return outranksNum / mentionNum;
+    }
+
+    /*
+    GIVEN: an Outcome o
+    RETURNS: true iff this competitor is mentioned by o
+     */
+    private Boolean isMention(Outcome o){
+        return o.isTie()? isMemberInTie(o) : 
+        o.winner().equals(this)|| o.loser().equals(this);
+    }
+
+    private Boolean defeatedOrTie(Outcome o){
+        if (o.isTie())
+            return isMemberInTie(o);
+        else
+        return o.winner().equals(this);
+    }
+
     @Override
     public boolean equals(Object obj) {
         return  n == ((Competitor)obj).name();
@@ -195,24 +283,31 @@ class Competitor1 implements Competitor {
 
     @Override
     public String toString() {
-        return name() + " "+ isOutranked;
+        return name() + " "+ isMarked;
     }
     public static void main(String[] args) {
-        Competitor A = competitor("A");
-        Competitor B = competitor("B");
-        Competitor C = competitor("C");
-        Competitor D = competitor("D");
         List<Outcome> outcomes = creatOutcomes(defeated("A", "B"), tie("B", "C"));
         List<Outcome> outcomes2 = creatOutcomes(defeated("A", "B"), defeated("B", "A"));
 
-        checkTrue(listEqual(A.outrankedBy(outcomes), Collections.emptyList()),"Test1");
-        checkTrue(listEqual(B.outrankedBy(outcomes2),Arrays.asList("A","B")),"Test2");
-        checkTrue(listEqual(C.outrankedBy(outcomes), Arrays.asList("A","B","C")),"Test3");
-        checkTrue(A.hasDefeated(B,outcomes),"Test 4");
-        checkTrue(B.hasDefeated(C,outcomes),"Test 5");
-        checkTrue(listEqual(.outranks(outcomes), Arrays.asList("C","B")),"Test6");
-        checkTrue(listEqual(B.outranks(outcomes2),Arrays.asList("A","B")),"Test7");
-        checkTrue(listEqual(C.outranks(outcomes), Arrays.asList("A","B","C")),"Test8");
+        checkTrue(listEqual(competitor("A").outrankedBy(outcomes),
+         Collections.emptyList()),"Test1");
+        checkTrue(listEqual(competitor("B").outrankedBy(outcomes2),
+            Arrays.asList("A","B")),"Test2");
+        checkTrue(listEqual(competitor("C").outrankedBy(outcomes),
+         Arrays.asList("A","B","C")),"Test3");
+        checkTrue(competitor("A").hasDefeated(competitor("B"),outcomes),"Test 4");
+        checkTrue(competitor("B").hasDefeated(competitor("C"),outcomes),"Test 5");
+
+        outcomes = creatOutcomes(defeated("A", "B"), tie("B", "C"),tie("C","D"));
+        outcomes2 = creatOutcomes(defeated("A", "B"), defeated("B", "A"));
+        checkTrue(listEqual(competitor("A").outranks(outcomes),
+            Arrays.asList("C","B","D")),"Test6");
+        checkTrue(listEqual(competitor("B").outranks(outcomes2),
+            Arrays.asList("A","B")),"Test7");
+        checkTrue(listEqual(competitor("C").outranks(outcomes),
+         Arrays.asList("B","C","D")),"Test8");
+        checkTrue(competitor("A").name() == "A","Test9");
+
         System.out.println("Test passed:" + testCount);
     }
 
@@ -221,7 +316,12 @@ class Competitor1 implements Competitor {
     Returns: true if they contain the same strings
      */
     private static Boolean listEqual(List<String> sl1,List<String> sl2){
-        return sl1.containsAll(sl2) && sl2.containsAll(sl1);
+        if(sl1.containsAll(sl2) && sl2.containsAll(sl1))
+            return true;
+        else {
+            System.out.println(sl1+"|"+sl2);
+            return false;
+        }
     }
 
     private static int testCount;
